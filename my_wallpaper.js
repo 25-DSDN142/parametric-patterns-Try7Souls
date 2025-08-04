c723a47bfa75892e834cca1d83c8914a522e4ca4
Try7Souls <orlando66cooper@gmail.com>
2025-08-04 13:16:53 +1200

    Github Save Problem
    github is not saving the right file

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -18,61 +18,22 @@ function setup_wallpaper(pWallpaper) {
 
 function wallpaper_background() {
   background(240, 255, 240); //light honeydew green colour
+
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   
 
- 
-//beginShape()
-//vertex(175,25);
-//bezierVertex(100, 80, 25, 25, 25, 25);
+line(150,105)
+//fill(255, 100, 0); // orange flame
+
+//beginShape();                    // bottom right
+//quadraticVertex(145,130,140,120);   // inner curve
+//quadraticVertex(155,100,150,120);        // outer curve
+//quadraticVertex(155,140,150,150);             // curve back to base
 //endShape();
 
-
-beginShape();
-vertex(50,0);
-quadraticVertex(50, 50, 0, 50);
-vertex(0,0);
-vertex(50,0)
-endShape();
-
-beginShape();
-vertex(150,0);
-quadraticVertex(150, 50, 200, 50);
-vertex(200,0);
-vertex(150,0);
-endShape();
-
-beginShape();
-vertex(50,200);
-quadraticVertex(50,150,0,150)
-vertex(0,200)
-vertex(50,200)
-endShape()
-
-beginShape();
-vertex(200,150)
-quadraticVertex(150,150,150,200)
-vertex(200,200)
-vertex(200,150)
-endShape()
-
-
-  
-//ears and head
-//fill(169,169,169)
-//circle(50,mouseheadY,50)
-//circle(150,mouseheadY,50)
-//triangle(50,mouseheadY,150,mouseheadY,100,150)
-//stroke(0)
-//fill(255)
-//strokeWeight(1.5)
-
-//eyes
-//ellipse(80,120,eyeSize+5,10)
-//ellipse(120,120,eyeSize+5,10)
-//strokeWeight(1)
+}
 
 //fill(0)
 //iner eyes
@@ -84,4 +45,3 @@ endShape()
 //ellipse(100,150,15)
 //strokeWeight(1)
 
-}
